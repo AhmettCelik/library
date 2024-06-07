14667f34fbc4598ccb1accdcd68516f651124a32
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-06-07 03:16:24 +0300

    Adding table and styles.
    adding table for display book objects on page.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const tableBody = document.querySelector("#books_table tbody");
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.read = read ? "readed" : "not readed yet";
   this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
       read ? "readed" : "not read yet"
@@ -16,3 +18,23 @@ function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+
+function showBooksOnTable() {
+  let tableRows = "";
+  myLibrary.forEach((book) => {
+    tableRows += `
+    <tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.read}</td>
+    </tr>`;
+    tableBody.innerHTML = tableRows;
+  });
+}
+
+showBooksOnTable();
